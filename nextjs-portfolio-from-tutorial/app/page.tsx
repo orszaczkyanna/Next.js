516ab6ce6db6ed193b532b2179ceb16babdf89bf
orszaczkyanna <orszaczky.anna@gmail.com>
2024-11-03 14:12:17 +0100

    feat: Add Recent Projects (Aceternity Portfolio)

--- nextjs-portfolio-from-tutorial/app/page.tsx
+++ nextjs-portfolio-from-tutorial/app/page.tsx
@@ -2,6 +2,7 @@ import { FaHome } from "react-icons/fa";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
