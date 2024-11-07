097686edab272c616927ac13ee8f02494075caeb
orszaczkyanna <orszaczky.anna@gmail.com>
2024-11-07 12:48:30 +0100

    feat: Add Moving Cards for Testimonials

--- nextjs-portfolio-from-tutorial/app/page.tsx
+++ nextjs-portfolio-from-tutorial/app/page.tsx
@@ -4,6 +4,7 @@ import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/Clients";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
