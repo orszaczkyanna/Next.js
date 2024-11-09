e274e7d426456a1e092c44e2479fe003a0a145c8
orszaczkyanna <orszaczky.anna@gmail.com>
2024-11-09 12:22:25 +0100

    feat: Add My Approach Section (Aceternity Portfolio)

--- nextjs-portfolio-from-tutorial/app/page.tsx
+++ nextjs-portfolio-from-tutorial/app/page.tsx
@@ -6,6 +6,7 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        {/* <Approach /> */}
       </div>
     </main>
   );
