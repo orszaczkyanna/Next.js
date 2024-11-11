62612a736b005e31fb6d888912106023a805806d
orszaczkyanna <orszaczky.anna@gmail.com>
2024-11-11 10:53:31 +0100

    feat: Add Footer (Aceternity Portfolio)

--- nextjs-portfolio-from-tutorial/app/page.tsx
+++ nextjs-portfolio-from-tutorial/app/page.tsx
@@ -7,6 +7,7 @@ import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -23,6 +24,7 @@ export default function Home() {
         <Clients />
         <Experience />
         {/* <Approach /> */}
+        <Footer />
       </div>
     </main>
   );
